06d550c7f7fb321b80472a745fdddeb3e7f106f8
Gonçalo Silva <goncalo.ribeiro.silva@live.com.pt>
2024-03-11 17:54:13 +0000

    tp2 done... quase :')

--- myServer.java
+++ myServer.java
@@ -1,12 +1,15 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 
 public class myServer{
 
-    private String adm_user = "user";
-    private String adm_password = "123";
-
     private File recieve = new File("recieve.txt");
+    private File usersfile = new File("users.txt");
 
     public static void main(String[] args) {
         System.out.println("servidor: main");
@@ -57,9 +60,16 @@ public class myServer{
                 String passwd = null;
                 
                 try {
+                    StringBuilder sb = new StringBuilder();
                     user = inStream.readUTF();
+                    sb.append(user + ":");
                     passwd = inStream.readUTF();
+                    sb.append(passwd);
                     System.out.println("thread: depois de receber a password e o user");
+                    if(!checkUsers(user, passwd)) {
+                        System.out.println("User inválido!");
+                        return; 
+                    }
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
@@ -91,6 +101,42 @@ public class myServer{
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private boolean checkUsers(String user, String pwd) {
+        Scanner sc = null;
+        try {
+            sc = new Scanner(usersfile);
+            while(sc.hasNextLine()) {
+                String[] user_check = sc.nextLine().split(":");
+                if(user.equals(user_check[0])) {
+                    if(!pwd.equals(user_check[1])) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            }
+            writeUser(user,pwd);
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+
+    private void writeUser(String user, String pwd) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(user + ":" + pwd);
+        try {
+            Files.write(Paths.get("users.txt"), sb.toString().getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
